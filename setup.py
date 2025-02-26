7076bde74bc0309e31bc46f31807b17faad0687d
DigitalKelvin <undocumentedscholar@gmail.com>
2025-02-26 17:42:41 -0600

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = """ Create asynchronous and asynchronous task
                     queues that run on a schedule... symantically """
 LONG_DESCRIPTION = (this_directory / "README.md").read_text()
